29dd7e983f78875e0e11ca8325232199adb1d17f
Cherry Zhou <42965594+Inpurple@users.noreply.github.com>
2019-05-17 16:12:15 +0800

    Create Two-way Merge Sort.py

---
+++ Algorithm_sort/Two-way Merge Sort.py
@@ -0,0 +1,48 @@
+"""
+4. 归并排序
+4.1 二路归并排序(Two-way Merge Sort)
+归并排序是建立在归并操作上的一种有效的排序算法。该算法是采用分治法（Divide and Conquer）的一个非常典型的应用。将已有序的子序列合并，得到完全有序的序列；即先使每个子序列有序，再使子序列段间有序。若将两个有序表合并成一个有序表，称为2-路归并。
+
+把长度为n的输入序列分成两个长度为n/2的子序列；
+对这两个子序列分别采用归并排序；
+将两个排序好的子序列合并成一个最终的排序序列。
+--------------------- 
+作者：傻了吧嗒 
+来源：CSDN 
+原文：https://blog.csdn.net/weixin_41571493/article/details/81875088 
+版权声明：本文为博主原创文章，转载请附上博文链接！
+"""
+    
+def merge(left,right,mid,nums):
+    i=left
+    j=mid+1
+    temp=[]
+    while i<=mid and j<=right:
+        if nums[i]<nums[j]:
+            temp.append(nums[i])
+            i=i+1
+        else:
+            temp.append(nums[j])
+            j=j+1
+    while i<=mid:
+            temp.append(nums[i])
+            i=i+1
+    while j<=right:
+            temp.append(nums[j])
+            j=j+1
+    
+    for k in range(len(temp)):
+        nums[left+k]=temp[k]
+        
+    
+def recur(left,right,nums):
+    if left>=right:
+        return
+    mid=(left+right)//2
+    recur(left,mid,nums)
+    recur(mid+1,right,nums)
+    merge(left,right,mid,nums)
+    
+array=[0, 0, 1, 3, 8, 0,100,-9,-30]
+recur(0,len(array)-1,array)
+print(array)
